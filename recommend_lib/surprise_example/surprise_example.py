179212f25ee66ce76814357d60ddd7b3e702f7c7
xinluomed_yuxuecheng <xinluomed_yuxuecheng@git.cloud.tencent.com>
2020-04-22 09:47:28 +0800

    1. 增加surprise库示例 2. 修改.gitignore文件。

---
+++ recommend_lib/surprise_example/surprise_example.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+@File    :   surprise_example.py
+@Time    :   2020/04/18 21:34:52
+@Author  :   Yu Xuecheng 
+@Version :   1.0
+@Contact :   yuxuecheng@xinluomed.com
+@License :   (C)Copyright 2020-2022, yuxuecheng
+@Desc    :   None
+'''
+
+# here put the import lib
+from surprise import SVD
+from surprise import Dataset
+from surprise.model_selection import cross_validate
+
+# Load the movielens-100k dataset (download it if needed).
+data = Dataset.load_builtin('ml-100k')
+
+# Use the famous SVD algorithm.
+algo = SVD()
+
+# Run 5-fold cross-validation and print results.
+cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
